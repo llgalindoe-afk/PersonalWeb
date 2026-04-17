7efbb5e7ac2ebaa82599efe0380881e46d081a24
Lluís Galindo <llgalindoe@gmail.com>
2026-04-17 12:56:42 +0200

    añadido resources js

---
+++ js/data/resources.js
@@ -0,0 +1,169 @@
+const resources = [
+  {
+    category: 'Documentación Oficial',
+    items: [
+      {
+        title: 'MDN Web Docs',
+        url: 'https://developer.mozilla.org/',
+        note: 'La referencia definitiva de HTML, CSS y JavaScript'
+      },
+      {
+        title: 'W3C Specifications',
+        url: 'https://www.w3.org/TR/',
+        note: 'Estándares web oficiales'
+      },
+      {
+        title: 'DevDocs',
+        url: 'https://devdocs.io/',
+        note: 'Documentación de múltiples tecnologías en un solo lugar'
+      },
+      {
+        title: 'Form Submit',
+        url: 'https://formsubmit.co',
+        note: 'envía emails sin necesidad de conectar un backend'
+      },
+    ]
+  },
+  {
+    category: 'Aprendizaje',
+    items: [
+      {
+        title: 'freeCodeCamp',
+        url: 'https://www.freecodecamp.org/',
+        note: 'Cursos gratuitos interactivos de desarrollo web'
+      },
+      {
+        title: 'JavaScript.info',
+        url: 'https://javascript.info/',
+        note: 'Tutorial moderno de JavaScript desde básico a avanzado'
+      },
+      {
+        title: 'CSS-Tricks',
+        url: 'https://css-tricks.com/',
+        note: 'Artículos, tutoriales y guías sobre CSS y frontend'
+      },
+      {
+        title: 'Web.dev',
+        url: 'https://web.dev/',
+        note: 'Guías de Google sobre performance, accesibilidad y PWAs'
+      },
+      {
+        title: 'Frontend Masters',
+        url: 'https://frontendmasters.com/',
+        note: 'Cursos en video de expertos (de pago)'
+      }
+    ]
+  },
+  {
+    category: 'Herramientas',
+    items: [
+      {
+        title: 'VS Code',
+        url: 'https://code.visualstudio.com/',
+        note: 'Editor de código más popular'
+      },
+      {
+        title: 'GitHub',
+        url: 'https://github.com/',
+        note: 'Hosting de repositorios Git y colaboración'
+      },
+      {
+        title: 'CodePen',
+        url: 'https://codepen.io/',
+        note: 'Playground online para HTML/CSS/JS'
+      },
+      {
+        title: 'Figma',
+        url: 'https://www.figma.com/',
+        note: 'Herramienta de diseño UI/UX colaborativa'
+      },
+      {
+        title: 'Netlify',
+        url: 'https://www.netlify.com/',
+        note: 'Deployment y hosting de sitios estáticos'
+      }
+    ]
+  },
+  {
+    category: 'Accesibilidad',
+    items: [
+      {
+        title: 'WCAG Guidelines',
+        url: 'https://www.w3.org/WAI/WCAG21/quickref/',
+        note: 'Referencia rápida de pautas de accesibilidad'
+      },
+      {
+        title: 'WebAIM',
+        url: 'https://webaim.org/',
+        note: 'Recursos y herramientas de accesibilidad web'
+      },
+      {
+        title: 'WAVE Tool',
+        url: 'https://wave.webaim.org/',
+        note: 'Evaluador de accesibilidad web'
+      },
+      {
+        title: 'Axe DevTools',
+        url: 'https://www.deque.com/axe/devtools/',
+        note: 'Extensión de navegador para testing de accesibilidad'
+      }
+    ]
+  },
+  {
+    category: 'Blogs & Comunidades',
+    items: [
+      {
+        title: 'Smashing Magazine',
+        url: 'https://www.smashingmagazine.com/',
+        note: 'Artículos de calidad sobre diseño y desarrollo web'
+      },
+      {
+        title: 'A List Apart',
+        url: 'https://alistapart.com/',
+        note: 'Contenido sobre estándares web y mejores prácticas'
+      },
+      {
+        title: 'Dev.to',
+        url: 'https://dev.to/',
+        note: 'Comunidad de desarrolladores con artículos y tutoriales'
+      },
+      {
+        title: 'Stack Overflow',
+        url: 'https://stackoverflow.com/',
+        note: 'Q&A para problemas de programación'
+      },
+      {
+        title: 'Frontend Focus',
+        url: 'https://frontendfoc.us/',
+        note: 'Newsletter semanal de noticias frontend'
+      }
+    ]
+  },
+  {
+    category: 'Inspiración & Design',
+    items: [
+      {
+        title: 'Awwwards',
+        url: 'https://www.awwwards.com/',
+        note: 'Showcase de diseño web de alta calidad'
+      },
+      {
+        title: 'Dribbble',
+        url: 'https://dribbble.com/',
+        note: 'Comunidad de diseñadores y portfolios'
+      },
+      {
+        title: 'Behance',
+        url: 'https://www.behance.net/',
+        note: 'Plataforma de portfolios creativos'
+      },
+      {
+        title: 'Google Fonts',
+        url: 'https://fonts.google.com/',
+        note: 'Biblioteca de fuentes web gratuitas'
+      }
+    ]
+  }
+];
+
+export default resources 
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { header, footer } from './templates/template.js';
 import darkMode from './utils/darkMode.js';
 import getSkills from './utils/skillList.js';
 import menu from './utils/menu.js';
+/*import getCheatsheets from './utils/cheatsheetsList.js';*/
+import { getResources } from './utils/resourcesGenerator.js';
 
 // Cargar templates de header y footer
 const siteHeader = document.querySelector('header');
